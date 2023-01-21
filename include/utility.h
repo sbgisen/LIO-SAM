f4973ab391e59743735d595c4c7a6054f4b3a538
alpaca-zip <zip.lottestr@gmail.com>
2023-01-21 17:37:24 +0900

    add lio_sam/useRGB param

--- include/utility.h
+++ include/utility.h
@@ -163,6 +163,9 @@ public:
     float globalMapVisualizationPoseDensity;
     float globalMapVisualizationLeafSize;
 
+    // colorize feature
+    bool useRGB;
+
     ParamServer()
     {
         nh.param<std::string>("/robot_id", robot_id, "roboat");
@@ -257,6 +260,8 @@ public:
         nh.param<float>("lio_sam/globalMapVisualizationSearchRadius", globalMapVisualizationSearchRadius, 1e3);
         nh.param<float>("lio_sam/globalMapVisualizationPoseDensity", globalMapVisualizationPoseDensity, 10.0);
         nh.param<float>("lio_sam/globalMapVisualizationLeafSize", globalMapVisualizationLeafSize, 1.0);
+
+        nh.param<bool>("lio_sam/useRGB", useRGB, false);
 
         usleep(100);
     }
